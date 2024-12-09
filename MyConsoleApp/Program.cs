13190e7a24a43cf0d368a92d90d0b2d3f15c800e
Fabián Camilo <Fabin Camilo@LAPTOP-FF3UB4CB>
2024-12-09 18:14:12 -0500

    Agregar archivos de proyecto.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary;
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Write("Hello, Writer!");
+
+            FiggleWriter FyWriter = new FiggleWriter();
+            FyWriter.Write("Hello, Figgle!");
+        }
+
+    }
+}
